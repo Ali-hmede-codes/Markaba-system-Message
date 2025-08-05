8e3d431baab9f5e6d8cd86b64ce6578b628b5baf
Ali-hmede-codes <sowftwarestudio@gmail.com>
2025-08-05 18:56:30 +0300

    run server update 4

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: 'whatsapp-message-system',
-    script: './src/backend/server.ts',
+    script: './dist/backend/server.ts',
     instances: 1,
     autorestart: true,
     watch: false,
